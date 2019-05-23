f8c0912cf9623fb55114a1ec5645d7fae09dc226
@anulman <anulman@gmail.com>
2019-05-23 16:58:12 -0400

    init twitter monitoring from data files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { cwd } from 'process';
 import { config } from 'dotenv';
 
 import { getResults } from './reddit';
+import { getTweets } from './twitter';
 import { SOURCES, notify } from './slack';
 import { loadFile } from './util/yaml';
 
@@ -17,10 +18,20 @@ async function initReddit(configPath = join(cwd(), 'data', 'subreddits.yml')) {
     let config = await loadFile(configPath);
 
     getResults(config, notify.bind(null, SOURCES.Reddit));
+  } catch(e) {
+    notify(e);
+  }
+}
 
+async function initTwitter(configPath = join(cwd(), 'data', 'twitter.yml')) {
+  try {
+    let config = await loadFile(configPath);
+
+    getTweets(config, notify.bind(null, SOURCES.Twitter));
   } catch(e) {
     notify(e);
   }
 }
 
 initReddit();
+initTwitter();
